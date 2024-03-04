15cb62fc7cb768671f8fc2b67b2f9a4745a89dea
Vlatko Anastasoski <anastasoskivlatko23@gmail.com>
2024-03-04 10:27:37 +0100

    Add NameAndSustrname.java

---
+++ src/main/java/NameAndSurname.java
@@ -0,0 +1,8 @@
+public class NameAndSurname {
+
+    public static void main(String[] args) {
+        String name = "Vlatko";
+        String surname = "Anastasoski";
+        System.out.println(name + "  " + surname);
+    }
+}
